2d4dfaf4623d5effc307332b2810cf36cb297374
ShagunSG <shaguneghatak@gmail.com>
2022-12-15 17:15:05 +0530

    dnav dark mode

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import './App.css';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm';
 import About from './components/About'
+import React, { useState } from 'react';
 
 function App() {
+  
+  const [mode, setMode] = useState ('white');
+
+  const toggleMode = () =>{
+    if (mode === 'white'){
+      setMode('black')
+    }
+    else {
+      setMode('white')
+    }
+  }
+  
   return (
     <>
-      <Navbar title="TechFest" aboutText="About us"/>
+      <Navbar mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
        <TextForm heading="Enter the text to analyze."/>
       </div>
